63087f3844a5678df25dfd5eb869975404037fcd
Yvonne <yvonnersz@gmail.com>
2021-01-06 00:02:22 -0800

    add registration and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: 'sessions#logged_out'
+  get :logged_in, to: 'sessions#logged_in'
+  
   resources :products, only: [:index, :show, :create]
   get '/product', to: 'products#search'
 
