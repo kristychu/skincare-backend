fe4d7355a602b6dbca43d088e94baed807b0ea72
kristychu <kristychu89@gmail.com>
2021-05-02 15:14:43 -0700

    config cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://kc-capstone2.herokuapp.com/'
+    origins 'kc-capstone2.herokuapp.com'
 
     resource '*',
       headers: :any,
